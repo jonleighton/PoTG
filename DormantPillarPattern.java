8e6bc5edf3f003a7478ebafebe09fd5707713031
Jon Leighton <j@jonathanleighton.com>
2019-09-18 12:19:07 +1000

    Refactor DormantPillarPattern
    We can use a heartbeat modulator rather than custom timing code.

--- DormantPillarPattern.java
+++ DormantPillarPattern.java
@@ -2,8 +2,11 @@ import heronarts.lx.LX;
 import heronarts.lx.LXCategory;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.parameter.CompoundParameter;
-import heronarts.lx.modulator.SinLFO;
+import heronarts.lx.parameter.FunctionalParameter;
 import heronarts.lx.modulator.LXModulator;
+import heronarts.lx.modulator.LXPeriodicModulator;
+import heronarts.lx.modulator.SinLFO;
+import heronarts.lx.modulator.LinearEnvelope;
 
 @LXCategory("Form")
 public class DormantPillarPattern extends PillarPattern {
@@ -15,10 +18,22 @@ public class DormantPillarPattern extends PillarPattern {
     new CompoundParameter("Probability", 0.5)
       .setDescription("The probability this pillar will light up");
 
-  private final LXModulator intensity = new SinLFO(0, 100, speed);
+  private final LXModulator intensity =
+    addModulator(
+      new SinLFO(0, 100, speed)
+        .setLooping(false)
+    );
 
-  private long nextIteration = 0;
-  private boolean active = false;
+  // Our 'heartbeat' loops twice as fast as the speed, so some pillars are
+  // fading in while others are fading out.
+  private final LXPeriodicModulator heartbeat = (LXPeriodicModulator)
+    startModulator(
+      new LinearEnvelope(0, 1, new FunctionalParameter() {
+        public double getValue() {
+          return speed.getValue() / 2.0;
+        }
+      }).setLooping(true)
+    );
 
   public DormantPillarPattern(LX lx) {
     this(lx, DEFAULT_PILLAR_NUMBER);
@@ -29,27 +44,17 @@ public class DormantPillarPattern extends PillarPattern {
 
     addParameter("speed", speed);
     addParameter("probability", probability);
+  }
 
-    addModulator(intensity);
+  public void onActive() {
+    heartbeat.trigger();
+    maybeActivate();
   }
 
   public void run(double deltaMs) {
-    long currentTime = System.currentTimeMillis();
+    if (heartbeat.loop()) maybeActivate();
 
-    // FIXME: somehow use modulators for this?
-    if (currentTime > nextIteration) {
-      active = Math.random() <= probability.getValuef();
-
-      if (active) {
-        intensity.trigger();
-        nextIteration = currentTime + (int) speed.getValuef();
-      } else {
-        intensity.stop();
-        nextIteration = currentTime + ((int) speed.getValuef() / 2);
-      }
-    }
-
-    setVertical(LXColor.gray(active ? intensity.getValuef() : 0));
+    setVertical(LXColor.gray(intensity.getValuef()));
     setAltarHead(headColor());
   }
 
@@ -59,5 +64,15 @@ public class DormantPillarPattern extends PillarPattern {
     } else {
       return LXColor.BLACK;
     }
+  }
+
+  private void maybeActivate() {
+    if (!intensity.isRunning() && shouldActivate()) {
+      intensity.trigger();
+    }
+  }
+
+  private boolean shouldActivate() {
+    return Math.random() <= probability.getValuef();
   }
 }
